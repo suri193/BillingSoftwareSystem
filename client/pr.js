d892a7404030e233f0efd868ac727721416fdd6e
suri193 <kr.surndra101@gmail.com>
2025-07-25 23:53:39 +0530

    Client folder created and basic setup done

---
+++ client/pr.js
@@ -0,0 +1,5 @@
+const message = "Hello, World!";
+
+// Extract from index 7 to 12 (exclusive)
+const slicedStr = message.slice(5); 
+console.log(slicedStr);
---
+++ client/src/pr.js
@@ -0,0 +1,5 @@
+var car = new Object(); 
+car["model"] = "Nano"; 
+car["company"] = "Tata"; 
+console.log("Model: " + car["model"]); 
+console.log("Company: " + car["company"]);
