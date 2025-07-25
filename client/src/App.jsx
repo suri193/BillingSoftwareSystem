576d4bc49f478968e40f618d9e93212c0c7424f0
suri193 <kr.surndra101@gmail.com>
2025-07-26 00:01:37 +0530

    client folder created and basic setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>Billing Software</h1>
-      {/* Your application components will go here */}
+      {/* Your application component */}
     </div>
   );
 }
